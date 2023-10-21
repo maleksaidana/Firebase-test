9c80883f1abde59c2ecbad4cdde4811ce8acae17
Malek <saidanamalek1997@gmail.com>
2023-10-21 13:56:53 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Home from "./Home";
 import { Routes, Route } from "react-router-dom"
 import Item from "./Item";
+import Header from "./Header";
 
 function App() {
   return (
-    <Routes>
-    <Route path="/" element={ <Home/> } />
-    <Route path="/item/:id" element={ <Item/> } />
-  </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/item/:id" element={<Item />} />
+        <Route path="/about" element={<h1> About </h1>} />
+      </Routes>
+    </>
 
   );
 }
